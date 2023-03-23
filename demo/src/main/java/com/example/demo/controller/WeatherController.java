0d26a0b86d3a54a8d3c023f29adcead4f2616292
Alexandra Lebedzeva <evtuhova15vek44@gmail.com>
2023-03-23 21:35:54 +0300

    app with json parse

--- demo/src/main/java/com/example/demo/controller/WeatherController.java
+++ demo/src/main/java/com/example/demo/controller/WeatherController.java
@@ -2,10 +2,10 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Weather;
 import com.example.demo.service.WeatherService;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.json.JsonParser;
-import org.springframework.boot.json.JsonParserFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.*;
@@ -13,14 +13,24 @@ import org.springframework.web.client.RestTemplate;
 
 import java.io.NotActiveException;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 public class WeatherController {
     @Autowired
     private WeatherService weatherService;
 
+    private static final ObjectMapper mapper = new ObjectMapper();
+
     private String url = "http://api.weatherapi.com/v1/current.json?key=267da6ee01ec4e0a95a162121232003&q=Minsk&aqi=no";
+
+    private double temp_c;
+    private String location;
+    private double wind_mph;
+    private double pressure_mb;
+    private int humidity;
+    private String last_updated;
+    private String condition;
+
 
     @GetMapping("/hello")
     public String hello() {
@@ -38,29 +48,55 @@ public class WeatherController {
         return weathers.stream().filter(weather -> weather.getLast_updated().equals(last_updated)).findFirst().orElseThrow(NotActiveException::new);
     }
 
-    @PostMapping(value = "/addWeather")
-    public Weather newWeather(@RequestBody Weather weather) {
-        return weatherService.addWeatherOnCurrentDate(weather);
-    }
-
     @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+
         return args -> {
             RestTemplate restTemplate = new RestTemplate();
             String resp = restTemplate.getForObject(url, String.class);
-            JsonParser springParser = JsonParserFactory.getJsonParser();
 
-            Map<String, Object> map = springParser.parseMap(resp);
-            String mapArray[] = new String[map.size()];
-            System.out.println("Items found: " + mapArray.length);
+            JsonNode root = mapper.readTree(resp);
 
-            int i = 0;
-            for (Map.Entry<String, Object> entry : map.entrySet()) {
-                System.out.println(entry.getKey() + " = " + entry.getValue());
-                i++;
+            JsonNode locationNode = root.path("location");
+            if (!locationNode.isMissingNode()) {
+                location = locationNode.path("region").asText();
+
+                System.out.println("name : " + locationNode.path("name").asText());
+                System.out.println("region : " + locationNode.path("region").asText());
             }
-            System.out.println(map.get("location"));
+
+            JsonNode currentNode = root.path("current");
+            if (!currentNode.isMissingNode()) {
+                temp_c = currentNode.path("temp_c").asDouble();
+                wind_mph = currentNode.path("wind_mph").asDouble();
+                pressure_mb = currentNode.path("pressure_mb").asDouble();
+                humidity = currentNode.path("humidity").asInt();
+                last_updated = currentNode.path("last_updated").asText();
+
+                System.out.println("temperature in celsius: " + currentNode.path("temp_c").asText());
+                System.out.println("wind mph: " + currentNode.path("wind_mph").asText());
+                System.out.println("pressure_mb: " + currentNode.path("pressure_mb").asText());
+                System.out.println("humidity: " + currentNode.path("humidity").asText());
+                System.out.println("last_updated: " + currentNode.path("last_updated").asText());
+            }
+
+            JsonNode conditionNode = root.path("condition");
+            if (!conditionNode.isMissingNode()) {
+                System.out.println("condition: " + conditionNode.path("text").asText());
+            }
         };
+    }
+
+    @PostMapping(value = "/addWeather")
+    public Weather newWeather(@RequestBody Weather weather) {
+        weather.setTemp_c((float) temp_c);
+        weather.setWind_mph((float) wind_mph);
+        weather.setPressure_mb((float) pressure_mb);
+        weather.setHumidity(humidity);
+        weather.setLocation(location);
+        weather.setLast_updated(last_updated);
+
+        return weatherService.addWeatherOnCurrentDate(weather);
     }
 
 }
